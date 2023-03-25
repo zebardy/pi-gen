ec012bf1c662360ea77481f595b9bf27edce5851
zebardy <zebardy@gmail.com>
2023-03-25 11:49:00 +0000

    make sure the next loop device is available before trying to make use of it

--- export-image/prerun.sh
+++ export-image/prerun.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+ensure_next_loopdev() {
+	local loopdev
+	loopdev="$(losetup -f)"
+	loopmaj="$(echo \"$loopdev\" | sed -E 's/.*loop([0-9]+)$/\1/')"
+	[[ -b "$loopdev" ]] || mknod "$loopdev" b 7 "$loopmaj"
+}
+
 if [ "${NO_PRERUN_QCOW2}" = "0" ]; then
 	IMG_FILE="${STAGE_WORK_DIR}/${IMG_FILENAME}${IMG_SUFFIX}.img"
 
@@ -35,7 +42,7 @@ if [ "${NO_PRERUN_QCOW2}" = "0" ]; then
 
 	echo "Creating loop device..."
 	cnt=0
-	until LOOP_DEV="$(losetup --show --find --partscan "$IMG_FILE")"; do
+	until ensure_next_loopdev && LOOP_DEV="$(losetup --show --find --partscan "$IMG_FILE")"; do
 		if [ $cnt -lt 5 ]; then
 			cnt=$((cnt + 1))
 			echo "Error in losetup.  Retrying..."

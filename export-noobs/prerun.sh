95377b6e2deff8fc7d69de845e58b40bca5b81a0
zebardy <zebardy@gmail.com>
2023-03-25 21:03:11 +0000

    Move partion loop device checking to after mount (they did not exist yet)

--- export-noobs/prerun.sh
+++ export-noobs/prerun.sh
@@ -37,19 +37,25 @@ mkdir -p "${NOOBS_DIR}"
 
 echo "Mounting partitions..."
 
+mount "$ROOT_DEV" "${STAGE_WORK_DIR}/rootfs"
+mount "$BOOT_DEV" "${STAGE_WORK_DIR}/rootfs/boot"
+
+echo "Ensure partition loop devices are accessable..."
+
 bootmaj="$(echo \"$BOOT_DEV\" | sed -E 's/.*loop([0-9]+p[0-9]+)$/\1/')"
 [[ -b "$BOOT_DEV" ]] || mknod "$BOOT_DEV" b 7 "$bootmaj"
 
 rootmaj="$(echo \"$ROOT_DEV\" | sed -E 's/.*loop([0-9]+p[0-9]+)$/\1/')"
 [[ -b "$ROOT_DEV" ]] || mknod "$ROOT_DEV" b 7 "$rootmaj"
 
-mount "$ROOT_DEV" "${STAGE_WORK_DIR}/rootfs"
-mount "$BOOT_DEV" "${STAGE_WORK_DIR}/rootfs/boot"
+echo "Configure OS and kernel..."
 
 ln -sv "/lib/systemd/system/apply_noobs_os_config.service" "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/apply_noobs_os_config.service"
 
 KERNEL_VER="$(zgrep -oPm 1 "Linux version \K(.*)$" "${STAGE_WORK_DIR}/rootfs/usr/share/doc/raspberrypi-kernel/changelog.Debian.gz" | cut -f-2 -d.)"
 echo "$KERNEL_VER" > "${STAGE_WORK_DIR}/kernel_version"
+
+echo "Tar partitions..."
 
 bsdtar --numeric-owner --format gnutar -C "${STAGE_WORK_DIR}/rootfs/boot" -cpf - . | xz -T0 > "${NOOBS_DIR}/boot.tar.xz"
 umount "${STAGE_WORK_DIR}/rootfs/boot"
@@ -58,5 +64,7 @@ bsdtar --numeric-owner --format gnutar -C "${STAGE_WORK_DIR}/rootfs" --one-file-
 if [ "${USE_QCOW2}" = "1" ]; then
 	rm "$ROOTFS_DIR/etc/systemd/system/multi-user.target.wants/apply_noobs_os_config.service"
 fi
+
+echo "Unmount image..."
 
 unmount_image "${IMG_FILE}"
